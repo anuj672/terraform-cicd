8d11f981f959d6d9456d0dc0c750fd668a3eb6ae
Anuj Naresh Chetwani <71286830+anuj672@users.noreply.github.com>
2023-11-01 19:04:34 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "myec2" {
-  ami           = "ami-0dbc3d7bc646e8516"
+  ami           = "ami-01bc990364452ab3e"
   instance_type = "t2.micro"
   tags = {
     Name = "my first ec2"
