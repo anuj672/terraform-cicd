49af38b71d28033f3115d86c3f8594614d21f1bd
achetwa <anujchetwani@gmail.com>
2023-11-03 03:44:05 -0400

    add bucket

--- provider.tf
+++ provider.tf
@@ -5,8 +5,11 @@ terraform {
       version = "5.24.0"
     }
   }
+  backend "s3" {
+    bucket = "github-oidc-terraform-anuj"
+    key    = "my-terraform-project"
+  }
 }
-
 provider "aws" {
   region = "us-east-1"
 }
